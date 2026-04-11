9dbf471654e82eb76bcbe6c26460417aedf551f9
Austin <105258719+Austin-540@users.noreply.github.com>
2026-04-11 21:59:01 +1200

    add a way to set the url and api key

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:async';
 
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: MyApp()));
 
 class MyApp extends StatefulWidget {
   @override
@@ -41,6 +42,52 @@ class _MyAppState extends State<MyApp> {
         ReceiveSharingIntent.instance.reset();
       });
     });
+
+    getSetupData();
+  }
+
+  String? apiKey;
+  String? gokapiUrl;
+  Future getSetupData() async {
+    FlutterSecureStorage storage = FlutterSecureStorage();
+    apiKey = await storage.read(key: "api_key");
+    gokapiUrl = await storage.read(key: "url");
+    setState(() {apiKey=apiKey;gokapiUrl=gokapiUrl;});
+    if (apiKey == null || gokapiUrl == null) {
+      if (!mounted) return;
+      print("Setup not completed!!! No API key or URL");
+    }
+  }
+
+  void editSavedData(String key) {
+    String? newValue;
+    showDialog(context: context, builder: (context) => 
+    Dialog(child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(autocorrect: false, decoration: InputDecoration(hint: Text("Your $key goes here...")),
+        onChanged: (value) => newValue = value,),
+      ),
+      ElevatedButton(onPressed: () async {
+        if (key == "url") {
+          setState(() {
+            gokapiUrl = newValue;
+          });
+        } else if (key == "api_key") {
+          setState(() {
+            apiKey = newValue;
+          });
+        }
+        FlutterSecureStorage storage = FlutterSecureStorage();
+        await storage.write(key: key, value: newValue);
+
+        if (!context.mounted) return;
+        Navigator.pop(context);
+      }, child: Text("Update $key"))
+    ],),
+    ));
   }
 
   @override
@@ -52,14 +99,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     const textStyleBold = const TextStyle(fontWeight: FontWeight.bold);
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Gokapi Quick Upload'),
         ),
         body: Center(
           child: Column(
             children: <Widget>[
+              ElevatedButton(onPressed: () => editSavedData("url"), child: Text("Gokapi URL: ${gokapiUrl.toString()}")),
+              ElevatedButton(onPressed: () => editSavedData("api_key"), child: Text("API Key: ${apiKey.toString()}")),
               Text("Shared files:", style: textStyleBold),
               Text(_sharedFiles
                       .map((f) => f.toMap())
@@ -67,7 +115,6 @@ class _MyAppState extends State<MyApp> {
             ],
           ),
         ),
-      ),
     );
   }
 }
